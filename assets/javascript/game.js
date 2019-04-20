7d44c071c6ee9b89ae1adfe48b37b01a68430d20
SamanthaAO <samanthaaorcutt@gmail.com>
2019-04-20 10:01:08 -0700

    made tiles created in jquery. created functions to dry up code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,168 +1,196 @@
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     //define variables
     var yourCharacter = "";
     var enemy = "";
     var ycHealth = "";
-    var ycInitialAttack ="";
+    var ycInitialAttack = "";
     var ycAttack = 0;
-    var enemyHealth ="";
-    var enemyDefense ="";
+    var enemyHealth = "";
+    var enemyDefense = "";
     var enemyCounter = 0;
 
 
     var characters = [
-        goat = {
+        {
             name: "Goat",
             initialAttack: 5,
             defense: 7,
-            health: 160,
-            image: "<img src='assets/images/goat.jpg' alt='goat' width='150' height='150'>",
+            health: 10,
+            image: "assets/images/goat.jpg",
         },
-    
-        cow = {
+        {
             name: "Cow",
             initialAttack: 10,
             defense: 5,
             health: 200,
-            image: "<img src='assets/images/cow.jpg' alt='cow' width='150' height='150'>",
+            image: "assets/images/cow.jpg",
         },
-    
-        sheep = {
+        {
             name: "Sheep",
             initialAttack: 4,
             defense: 6,
             health: 180,
-            image: "<img src='assets/images/sheep.jpg' alt='sheep' width='150' height='150'>",
+            image: "assets/images/sheep.jpg",
         },
-    
-        pig = {
+        {
             name: "Pig",
             initialAttack: 8,
             defense: 9,
             health: 210,
-            image: "<img src='assets/images/pig.jpg' alt='pig' width='150' height='150'>",
+            image: "assets/images/pig.jpg",
         },
     ];
-    function health(){
-    for(var j=0; j<characters.length; j++){
-    $(".card-footer" +j).text(characters[j].health + "HP");
+
+    function displayCharacters() {
+        var html = "";
+        characters.forEach(function (character, i) {
+            var name = character.name.toLowerCase();
+            html += `<button class="characterTile" id="${name}Tile" value="${i}">
+                        <div class="card text-center">
+                            <div class="card-header">
+                            ${character.name}
+                            </div>
+
+                            <div class="card-body">
+                                <img src="${character.image}" alt="${name}" width="150" height="150">
+                            </div>
+
+                            <div class="card-footer${i}">
+                            Health
+                            </div>
+                        </div>
+                    </button>`;
+
+        });
+
+        $("#characterRow").html(html);
     }
+    displayCharacters();
+
+
+    function health() {
+        for (var j = 0; j < characters.length; j++) {
+            $(".card-footer" + j).text(characters[j].health + "HP");
+        }
     }
     health();
 
-    // console.log(characters[1].defense);
+    function fighterStats(isEnemy, obj){
+        var div = (isEnemy) ? "#enemyDiv" : "#ycDiv";
+        var msg = (isEnemy) ? "Current Oppenent: " : "Your Character: ";
+        var myClass = (isEnemy) ? "enemy" : "yc";
+        var id = $(obj).val();
+        $(div).append(`<h1>${msg}</h1>`);
+        $(div).append(obj);
+        $(obj).addClass(myClass);
+        if (isEnemy)
+        {
+            enemyHealth = characters[id].health;
+            enemyDefense = characters[id].defense;
+            enemyName = characters[id].name;
+            enemy = id;
+        }
+        else {
+            ycHealth = characters[id].health;
+            ycInitialAttack = characters[id].initialAttack;
+            ycAttack = ycInitialAttack;
+            ycName = characters[id].name;
+            yourCharacter = id;
+        }
+
+    }
 
     //start all character tiles in the begining div
     $("#begin").append($(".characterTile"));
     $("#restartBox").text("Click on the character that you would like to play as first. Then click to select your first opponent. CHOOSE WISELY!")
 
-    
 
-    $(".characterTile").on("click", function() {
+
+    $(".characterTile").on("click", function () {
         //assign yc
-        if(yourCharacter === "" && enemy === ""){
-        $('#ycDiv').append("<h1>Your Character:</h1>");
-        $('#ycDiv').append(this);
-        $(this).addClass('yc');
-            var i = $(this).val();
-                ycHealth = characters[i].health;
-                ycInitialAttack = characters[i].initialAttack;
-                ycAttack = ycInitialAttack;
-                ycName = characters[i].name;
-                yourCharacter = i;
-        $('#begin').prepend("<h1>Future Opponents:</h1>");
-            
-        //create restart button
+        if (yourCharacter === "" && enemy === "") {
+
+            fighterStats(false, this);
+
+            $('#begin').prepend("<h1>Future Opponents:</h1>");
+
+            //create restart button
             $("#restartBox").empty();
             $('#restartBox').append("<button type='button' class='btn btn-danger restartBtn'>New Game</button><br>")
-            $('.restartBtn').click( function() {
+            $('.restartBtn').click(function () {
                 location.reload();
             });
 
             // create attack button
             $('#attackBox').append("<button type='button' class='btn btn-danger attackBtn'>Attack</button><br>")
-            $('.attackBtn').on("click", function() {
-                
-                
-                if(ycHealth >= 0 && enemyHealth >= 0){
-                    enemyHealth = enemyHealth - ycAttack;
-                    ycHealth = ycHealth - enemyDefense;
+            
 
-                    $('#narration').text("You attacked " + enemyName + " for " + ycAttack + " damage. " + enemyName + 
-                    " attacked back for " + enemyDefense + " damage." );
+            //checks
+            //  indexValue = parseInt(indexValue);  <- this might need to be used if issues with subtrracting later down the line. 
 
-
-                    ycAttack = ycInitialAttack + ycAttack;
-                    $(".card-footer" +yourCharacter).text(ycHealth + "HP");
-                    $(".card-footer" + enemy).text(enemyHealth + "HP");
-                        console.log(ycHealth);
-                        console.log(enemyHealth);
-                    
-                        if (enemyHealth <=0){
-                            $('#narration').text("You have defeated " + enemyName + "!!! Select your next opponent." );
-                            $('#enemyDiv').empty(); 
-                            enemy = "";
-                        }
-                    
-
-                }
-                else if (enemyHealth <=0){
-                    $('#narration').text("You have defeated " + enemyName + "!!! Select your next opponent." );
-                    $('#enemyDiv').empty(); 
-                    enemyCounter++;
-                    enemy = "";
-                        // if (enemyCounter === characters.length-1){
-                        //     $("#begin").empty();
-                        //     $("#begin").text("You beat all possible opponents!!! Click new game to restart.");
-                        // }
-        
-                }
-                else{
-                    $('#narration').text("You have been defeated :(");
-                    $('#ycDiv').empty();  
-
-                    
-        
-                }
-        
-        
-            })
-        
- 
-        //checks
-        //  indexValue = parseInt(indexValue);  <- this might need to be used if issues with subtrracting later down the line. 
-        
         }
         //assign enemy 
-        else if(enemy ===""){
-            $('#enemyDiv').append("<h1>Current Opponent:</h1>");
-            $('#enemyDiv').append(this); 
-            $(this).addClass('enemy');
-                var j = $(this).val();
-                    enemyHealth = characters[j].health;
-                    enemyDefense = characters[j].defense;
-                    enemyName = characters[j].name;
-                    enemy = j;
-                        
-        
-
-
-
-                    
-        
-            }
+        else if (enemy === "") {
+            fighterStats(true, this);
+        }
     })
 
-    
+    //create click event for attack button
+    $('#attackBox').on("click", ".attackBtn", function () {
+        if (ycHealth >= 0 && enemyHealth >= 0 && enemy !== "") {
+            enemyHealth = enemyHealth - ycAttack;
+            ycHealth = ycHealth - enemyDefense;
 
-    
-    
+            $('#narration').text("You attacked " + enemyName + " for " + ycAttack + " damage. " + enemyName +
+                " attacked back for " + enemyDefense + " damage.");
 
-   
 
-  
+            ycAttack = ycInitialAttack + ycAttack;
+            $(".card-footer" + yourCharacter).text(ycHealth + "HP");
+            $(".card-footer" + enemy).text(enemyHealth + "HP");
+            console.log(ycHealth);
+            console.log(enemyHealth);
+
+            // if (enemyHealth <= 0) {
+            //     $('#narration').text("You have defeated " + enemyName + "!!! Select your next opponent.");
+            //     $('#enemyDiv').empty();
+            //     enemy = "";
+            // }
+
+
+        }
+        
+        //check to see if anyone has been defeated
+        if (enemyHealth <= 0 && enemy !== "") {
+
+            $('#narration').text("You have defeated " + enemyName + "!!! Select your next opponent.");
+            $('#enemyDiv').empty();
+            enemyCounter++;
+            enemy = "";
+            if (enemyCounter === characters.length-1){
+                $("#begin").empty();
+                $("#begin").text("You beat all possible opponents!!! Click new game to restart.");
+            }
+        }
+
+        else if(ycHealth <= 0  && enemy !== "" ) {
+            $('#narration').text("You have been defeated :(");
+            $('#ycDiv').empty(); 
+        }
+
+    })
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -180,29 +208,3 @@ $(document).ready(function() {
 
 
 
-    // $('#begin').append('<div id="goat" />');
-    // $('#goat').append(characters[0].name + "<br>");
-    // $('#goat').append(characters[0].image + "<br>");
-    // $('#goat').append(characters[0].health + "<br>");
-
-    // $('#begin').append('<div id="cow" />');
-    // $('#cow').append(characters[1].name + "<br>");
-    // $('#cow').append(characters[1].image + "<br>");
-    // $('#cow').append(characters[1].health + "<br>");
-
-    // $('#begin').append('<div id="sheep" />');
-    // $('#sheep').append(characters[2].name + "<br>");
-    // $('#sheep').append(characters[2].image + "<br>");
-    // $('#sheep').append(characters[2].health + "<br>");
-
-    // $('#begin').append('<div id="pig" />');
-    // $('#pig').append(characters[3].name + "<br>");
-    // $('#pig').append(characters[3].image + "<br>");
-    // $('#pig').append(characters[3].health + "<br>");
-
-    // $("#begin").append(" cow ");
-    // $("#begin").append(" sheep ");
-    // $("#begin").append(" pig ");
-    
-    // $("#battleField").append(".characterTile");
-    // $("#barricks").append(".characterTile");
